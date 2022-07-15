6a5fc0b0a9b3af777d62c1e094bbe5cddd01955d
brenu <bvsousa64@gmail.com>
2022-07-14 23:14:12 -0300

    :rocket: Add api route for public flowcharts

--- api/app/Controllers/Http/FlowchartsController.ts
+++ api/app/Controllers/Http/FlowchartsController.ts
@@ -1,12 +1,99 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Database from '@ioc:Adonis/Lucid/Database'
+import Comment from 'App/Models/Comment'
 import Flowchart from 'App/Models/Flowchart'
+import StudentFlowchart from 'App/Models/StudentFlowchart'
+import Subject from 'App/Models/Subject'
+import User from 'App/Models/User'
 
 export default class FlowchartsController {
-    public async index({ response }: HttpContextContract) {
-        const flowcharts = await Flowchart.query()
-            .select(["id", "name"])
-            .orderBy("name");
+  public async index({ response }: HttpContextContract) {
+    const flowcharts = await Flowchart.query().select(['id', 'name']).orderBy('name')
 
-        return response.status(200).json(flowcharts);
+    return response.status(200).json(flowcharts)
+  }
+
+  public async show({ request, response }: HttpContextContract) {
+    const studentId = request.param('student-id')
+    const flowchartId = request.param('flowchart-id')
+
+    const flowchart = await Flowchart.find(flowchartId)
+
+    if (!flowchart) {
+      return response.status(404)
     }
+
+    const student = await User.find(studentId)
+
+    if (!student) {
+      return response.status(404)
+    }
+
+    const studentFlowchart = await StudentFlowchart.query()
+      .where('student_id', studentId)
+      .andWhere('flowchart_id', flowchartId)
+      .first()
+
+    if (!studentFlowchart) {
+      return response.status(404)
+    }
+
+    if (!studentFlowchart.is_public) {
+      return response.status(404)
+    }
+
+    const semesters = await Subject.query()
+      .select('semester')
+      .where('flowchart_id', flowchart.id)
+      .andWhere('is_archived', false)
+      .groupBy('semester')
+      .orderBy('semester')
+
+    const subjects: object = {}
+
+    for (let item of semesters) {
+      subjects[item.$original.semester] = (
+        await Database.rawQuery(`SELECT
+                subjects.id, name, code, summary, status, is_archived, theoretical_load,
+                practical_load, professor, objective, methodology, assessment
+                FROM subjects
+                INNER JOIN student_subjects ON subjects.id = student_subjects.subject_id
+                WHERE student_subjects.student_id = ${studentId}
+                AND semester = ${item.$original.semester}
+                ORDER BY name
+              `)
+      ).rows
+
+      for (let subject of subjects[item.$original.semester]) {
+        const prerequisites = await Database.rawQuery(`
+                  SELECT code FROM prerequisites
+                  INNER JOIN subjects ON subjects.id = prerequisites.prerequisite_id
+                  WHERE prerequisites.subject_id = ${subject.id}
+                  AND subjects.id != ${subject.id}
+                `)
+
+        subject.comments = []
+
+        if (studentFlowchart.has_public_comments) {
+          const comments = await Comment.query()
+            .select(['content', 'created_at'])
+            .where('subject_id', subject.id)
+            .andWhere('owner_id', studentFlowchart.id)
+
+          subject.prerequisites = await prerequisites.rows.map(
+            (prerequisite: Subject) => prerequisite.code
+          )
+
+          subject.comments = comments
+        }
+      }
+    }
+
+    return response.status(200).json({
+      flowchart_name: flowchart.name,
+      student_id: studentId,
+      username: student.username,
+      subjects,
+    })
+  }
 }
--- api/start/routes.ts
+++ api/start/routes.ts
@@ -20,68 +20,64 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/flowcharts', 'FlowchartsController.index');
+Route.get('/flowcharts', 'FlowchartsController.index')
 
 Route.post('login', async ({ auth, request, response }) => {
   try {
-    const username = request.input('username');
-    const password = request.input('password');
+    const username = request.input('username')
+    const password = request.input('password')
 
-    const result = await auth.use('api').attempt(username, password);
+    const result = await auth.use('api').attempt(username, password)
 
     return response.json({
       token: result.token,
-      role: result.user.$original.role
-    });
+      role: result.user.$original.role,
+    })
   } catch (error) {
-    return response.status(400).json({ message: "Os dados não coincidem" });
+    return response.status(400).json({ message: 'Os dados não coincidem' })
   }
-});
+})
 
-Route.post('forgot', "ForgotController.store");
-Route.put('reset/:token', "ForgotController.update");
+Route.post('forgot', 'ForgotController.store')
+Route.put('reset/:token', 'ForgotController.update')
 
-Route.post('student', "StudentsController.store");
-Route.post('coordinator', "CoordinatorsController.store");
-Route.post('form-validation/', "FormController.store");
-
+Route.post('student', 'StudentsController.store')
+Route.post('coordinator', 'CoordinatorsController.store')
+Route.post('form-validation/', 'FormController.store')
+Route.get('/flowchart/:student-id/:flowchart-id', 'FlowchartsController.show')
 
 Route.group(() => {
   Route.group(() => {
-    Route.get('student/:flowchart-id', "StudentsController.show");
-    Route.put('student-subject/:subject-id', "StudentSubjectsController.update");
-    Route.get('student-flowcharts', "StudentSubjectsController.index");
+    Route.get('student/:flowchart-id', 'StudentsController.show')
+    Route.put('student-subject/:subject-id', 'StudentSubjectsController.update')
+    Route.get('student-flowcharts', 'StudentSubjectsController.index')
 
-    Route.post('comments', "CommentsController.store");
-    Route.delete('comments/:comment-id', "CommentsController.destroy");
-    Route.get('student/privacy/:flowchart-id', 'PrivacySettingsController.show');
-    Route.put('student/privacy/:flowchart-id', 'PrivacySettingsController.update');
-
+    Route.post('comments', 'CommentsController.store')
+    Route.delete('comments/:comment-id', 'CommentsController.destroy')
+    Route.get('student/privacy/:flowchart-id', 'PrivacySettingsController.show')
+    Route.put('student/privacy/:flowchart-id', 'PrivacySettingsController.update')
   }).middleware(async ({ request, response }, next) => {
-    const user = request.session_user;
+    const user = request.session_user
 
-    if (!user || user.role !== "student") {
-      return response.status(403);
+    if (!user || user.role !== 'student') {
+      return response.status(403)
     }
 
-    await next();
-  });
+    await next()
+  })
 
   Route.group(() => {
-
-    Route.get('coordinator/subject', 'CoordinatorSubjectsController.index');
-    Route.post('coordinator/subject', 'CoordinatorSubjectsController.store');
-    Route.put('coordinator/subject/:subject-id', 'CoordinatorSubjectsController.update');
-    Route.delete('coordinator/subject/:subject-id', 'CoordinatorSubjectsController.destroy');
-
+    Route.get('coordinator/subject', 'CoordinatorSubjectsController.index')
+    Route.post('coordinator/subject', 'CoordinatorSubjectsController.store')
+    Route.put('coordinator/subject/:subject-id', 'CoordinatorSubjectsController.update')
+    Route.delete('coordinator/subject/:subject-id', 'CoordinatorSubjectsController.destroy')
   }).middleware(async ({ request, response }, next) => {
-    const user = request.session_user;
+    const user = request.session_user
 
-    if (!user || user.role !== "coordinator") {
-      return response.status(403);
+    if (!user || user.role !== 'coordinator') {
+      return response.status(403)
     }
 
-    await next();
-  });
-
-}).middleware('auth:api');
+    await next()
+  })
+}).middleware('auth:api')
